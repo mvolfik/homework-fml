1ad7251138cc0aa1aad1f6e8a908b33a8d7e94c8
Matěj Volf <mat.volfik@gmail.com>
2020-09-24 17:14:48 +0200

    Base template, init material design

--- homework_fml/__init__.py
+++ homework_fml/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 def create_app():
@@ -14,11 +14,20 @@ def create_app():
         }
     )
     # endregion
+
     # region db
     from .db import db, migrate
 
     db.init_app(app)
     migrate.init_app(app, db)
+
+    # endregion
+
+    # region homepage
+    @app.route("/")
+    def homepage():
+        return render_template("homepage.html")
+
     # endregion
 
     return app
