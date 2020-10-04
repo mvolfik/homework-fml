b2f4b7bb6692b17f7609ab9cf51a6e6799d3a7fe
Matěj Volf <mat.volfik@gmail.com>
2020-10-04 14:27:03 +0200

    Comment

--- homework_fml/db.py
+++ homework_fml/db.py
@@ -12,4 +12,6 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(CIText, unique=True, nullable=False)
     hash = db.Column(db.String, nullable=False)
-    email_verification_token = db.Column(db.String, unique=True)
+    email_verification_token = db.Column(
+        db.String, unique=True
+    )  # simplest solution, once it's None, the user is verified
