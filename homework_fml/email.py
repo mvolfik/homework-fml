931d2c0e3589f9a35bc0b9347d84ed0a7044ebdd
Matěj Volf <mat.volfik@gmail.com>
2020-10-04 14:28:09 +0200

    email.py: split into multiple functions and add sentry perf-span hints

--- homework_fml/email.py
+++ homework_fml/email.py
@@ -4,19 +4,10 @@ from smtplib import SMTP_SSL
 from typing import Optional
 
 import dkim
+from sentry_sdk import start_span
 
 
-def send_mail(
-    to: str, subject: str, plaintext_body: str, html_body: Optional[str] = None,
-) -> None:
-    """Send an email, optionally with multipart HTML body.
-
-    :param to: Address to send the email to.
-    :param subject: Subject of the message.
-    :param plaintext_body: Plain text version of the email body.
-    :param html_body: Optional; If included, the message will be multipart/alternative.
-    :rtype: None
-    """
+def _create_message(to, subject, plaintext_body, html_body):
     msg = EmailMessage()
     msg["Subject"] = subject
     msg["From"] = os.environ["EMAIL_SENDER"]
@@ -26,11 +17,15 @@ def send_mail(
     if html_body is not None:
         msg.add_alternative(html_body, subtype="html")
 
-    # I'm too poor for actual email hosting, so the mails are received by improvmx.com
-    # and forwarded to my personal email. As for sending, this is done by a small trick
-    # – I use my personal email provider to send the mail with the From header set to
-    # @homework-f.ml. That works, but some clients treat it as untrustworthy, so I add
-    # a DKIM signature to prove it is still sent by owner of the "impersonated" domain
+    return msg
+
+
+# I'm too poor for actual email hosting, so the mails are received by improvmx.com
+# and forwarded to my personal email. As for sending, this is done by a small trick
+# – I use my personal email provider to send the mail with the From header set to
+# @homework-f.ml. That works, but some clients treat it as untrustworthy, so I add
+# a DKIM signature to prove it is still sent by owner of the "impersonated" domain
+def _sign_message(msg):
     msg["DKIM-Signature"] = (
         dkim.sign(
             message=msg.as_bytes(),
@@ -45,7 +40,28 @@ def send_mail(
         .replace("\r\n", "")
     )
 
+
+def _send_message(msg):
     s = SMTP_SSL(os.environ["SMTP_SERVER"])
     s.login(os.environ["SMTP_LOGIN"], os.environ["SMTP_PWD"])
     s.send_message(msg)
     s.quit()
+
+
+def send_mail(
+    to: str, subject: str, plaintext_body: str, html_body: Optional[str] = None,
+) -> None:
+    """Send an email, optionally with multipart HTML body.
+
+    :param to: Address to send the email to.
+    :param subject: Subject of the message.
+    :param plaintext_body: Plain text version of the email body.
+    :param html_body: Optional; If included, the message will be multipart/alternative.
+    :rtype: None
+    """
+
+    with start_span(op="email", description="Send an email"):
+        msg = _create_message(to, subject, plaintext_body, html_body)
+        _sign_message(msg)
+        with start_span(op="smtp", description="Send the message"):
+            _send_message(msg)
